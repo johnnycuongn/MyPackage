96e67b91b0722a45343bcae3615ab65d384204ca
johnnycuongn <cuongdn2001@gmail.com>
2020-11-13 16:30:11 +1100

    Initial Commit

---
+++ Sources/MyPackage/MyPackage.swift
@@ -0,0 +1,11 @@
+
+public class SwiftPackage {
+    public init() {
+    }
+    
+    public func connectPackage() {
+        print("Connect Package Success")
+    }
+    
+    var text = "Hello, World!"
+}
